44952c0240d8f51c8dd2d38e7eaf4a8d645e5c43
kassike <kaikems27@outlook.com>
2023-10-24 21:54:15 -0300

    2 exercicio

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -5,4 +5,11 @@ insira uma mensagem na tabela Auditoria informando a data e hora da inserção.*
 
 create trigger inserir_novo_cliente AFTER INSERT on clientes for each row 
 INSERT INTO auditoria 
-values ('novo cliente adicionado as '); ;
+values ('novo cliente adicionado as '); 
+
+/*Antes de excluir um cliente da tabela Clientes, 
+crie um trigger que insira uma mensagem na tabela Auditoria informando sobre a tentativa de exclusão.*/
+
+create trigger deletar_cliente before delete on clientes for each row 
+INSERT INTO auditoria 
+values ('houve uma tentativa de deletar um cliente as '); 
