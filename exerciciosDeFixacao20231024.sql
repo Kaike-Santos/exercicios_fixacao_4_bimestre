519eff86926df41748a09b0f5faac12688660e72
kassike <kaikems27@outlook.com>
2023-10-24 21:56:19 -0300

    3 exercicio

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -13,3 +13,10 @@ crie um trigger que insira uma mensagem na tabela Auditoria informando sobre a t
 create trigger deletar_cliente before delete on clientes for each row 
 INSERT INTO auditoria 
 values ('houve uma tentativa de deletar um cliente as '); 
+
+/*Após atualizar o nome de um cliente na tabela Clientes,
+ insira uma mensagem na tabela Auditoria mostrando o nome antigo e o novo nome.*/
+
+create trigger atualizar_nome_cliente after UPDATE on clientes for each row 
+INSERT INTO auditoria 
+values ('houve uma alteração no nome de um cliente , o nome antigo era ', old.nome ,'e agora é ', NEW.nome); 
